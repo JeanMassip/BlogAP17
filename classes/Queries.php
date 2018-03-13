854731f7652892a7641e7d225443f0b1fc7f44d6
JeanM <jean.massip@viacesi.fr>
2018-03-13 14:30:35 +0100

    Heritage Classes and Auto - Loader

---
+++ classes/Queries.php
@@ -0,0 +1,15 @@
+<?php
+
+class Queries
+{
+    // DSN : Data Source Name
+    private $dsn = "mysql:dbname=ap;host=localhost;charset=utf-8";
+    private $user = 0;
+    private $password = 0;
+    private $db;
+
+    public function __construct()
+    {
+
+    }
+}
